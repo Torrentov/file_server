10775d60eaf37c4c082739ff7ef1a99173acb35c
Torrentov <bekyan.artem@gmail.com>
2018-11-12 20:52:25 +0300

    now works with files with spaces or special symbols

--- upload/views.py
+++ upload/views.py
@@ -4,24 +4,39 @@ from django.template import RequestContext
 from django.db import models
 from .forms import UploadFileForm
 from .models import Document
+from time import time
 from server.vars import PATH, TMP_PATH, SITE
 import os
 from shutil import copyfile
 
 # Create your views here.
 
+class crutch():
+    name = 'crutch'
+
+
 def index(request):
     if not request.user.is_authenticated or request.session['auth'] != 1:
         return HttpResponseRedirect('/')
     needed_path = PATH + 'static/' + request.GET['folder']
     os.chdir(TMP_PATH)
+    name = ''
+    k = 'crutch'
+    request.FILES[k] = crutch()
+    for key in request.FILES:
+        if key != 'crutch':
+            name = request.FILES[key].name
+            k = key
+    curr = str(time())
+    real_name = request.FILES[k].name
+    request.FILES[k].name = curr
     current_site = SITE + request.GET['folder']
     form = UploadFileForm(request.POST, request.FILES)
     if form.is_valid():
         form.save()
-        current = str(form.cleaned_data['docfile'])
+        current = real_name
         files = os.listdir(path=needed_path)
-        current_file = TMP_PATH + current
+        current_file = TMP_PATH + curr
         i = 1
         while current in files:
             if current.count('(%s)' % str(i)) > 0:
@@ -31,7 +46,7 @@ def index(request):
                 current = current.split('.')
                 current[0] += '(%s)' % str(i)
                 current = current[0] + '.' + current[1]
-        needed_path += current
+        needed_path += current.replace(curr, '')
         os.rename(current_file, PATH + needed_path.replace(PATH, ''))
         return HttpResponseRedirect(current_site.replace(' ', '%20'))
     else:
