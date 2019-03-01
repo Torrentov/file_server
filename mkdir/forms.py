25966c8475dd17db19a461372d7c842436db5ca7
Torrentov <bekyan.artem@gmail.com>
2019-03-01 18:05:03 +0300

    better mkdir

--- mkdir/forms.py
+++ mkdir/forms.py
@@ -1,18 +1,8 @@
-from django.forms import ModelForm, Textarea
-from .models import Text
-from django.utils.translation import ugettext_lazy as _
+from django import forms
 
-class NewFolderForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(NewFolderForm, self).__init__(*args, **kwargs)
-        self.label_suffix = ''
-    class Meta:
-        model = Text
-        labels = {
-            'folder': _(''),
-        }
-        widgets = {
-            'folder': Textarea(attrs={'style': 'height:30px; font-size:20px; '
-                                        'font-family:"Calibri"; margin-top:10px'})
-        }
-        fields = ['folder']
+class NewFolderForm(forms.Form):
+    folder = forms.CharField(label='', label_suffix='',
+                             widget=forms.TextInput(attrs={'style':
+                                        'height:30px; font-size:20px; width:300px'
+                                        'font-family:"Calibri"; margin-top:10px'}),
+                             max_length=128)
