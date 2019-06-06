e271dba13b09dac18659e3c0d6c64937e3e69f66
Torrentov <bekyan.artem@gmail.com>
2019-06-06 14:02:09 +0300

    bugs fixed

--- server/vars.py
+++ server/vars.py
@@ -1,12 +1,12 @@
-SERVER_PATH = '/Users/Artem/Desktop/school_server/server'
-#SERVER_PATH = '/home/Torrentov/file_server'
+#SERVER_PATH = '/Users/Artem/Desktop/school_server/server'
+SERVER_PATH = '/home/Torrentov/file_server'
 
 PATH = SERVER_PATH + '/files/'
 TMP_PATH = PATH + 'tmp/'
 FILE_PATH = PATH + 'static/needed_files'
 
-#DEFAULT_SITE = 'http://torrentov.pythonanywhere.com/files'
-DEFAULT_SITE = 'http://127.0.0.1:8000/files'
+DEFAULT_SITE = 'http://torrentov.pythonanywhere.com/files'
+#DEFAULT_SITE = 'http://127.0.0.1:8000/files'
 SITE = DEFAULT_SITE + '?folder='
 SITE_UPLOAD = DEFAULT_SITE + '/upload'
 SITE_RENAME = DEFAULT_SITE + '/rename'
@@ -16,5 +16,5 @@ SITE_CREATE_FOLDER = DEFAULT_SITE + '/mkdir'
 FILE_DELETE = SITE_DELETE_FILE + '/file_remove?delete='
 FOLDER_DELETE = SITE_DELETE_FOLDER + '/folder_remove?delete='
 
-#FONT_PATH = "~/.fonts/8277.ttf"
-FONT_PATH = "/Users/Artem/Downloads/8277.ttf"
+FONT_PATH = "~/.fonts/8277.ttf"
+#FONT_PATH = "/Users/Artem/Downloads/8277.ttf"
